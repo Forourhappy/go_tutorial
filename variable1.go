31a6a95522ba6e911f0cb7fa93e9e9e874110f08
Foh <foroh1504@gmail.com>
2025-12-01 19:24:58 +0900

    first commit

---
+++ variable1.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	/**
+	기본 초기화
+	정수타입 : 0, 실수(소수점) : 0.0, 문자열 : "", Boolean : true, false
+	변수명 : 숫자 첫글자x, 대소문자 구분O, 문자, 숫자, 밑줄, 특수기호 사용 가능
+	변수 및 상수 : 함수 내외 사용 가능
+	*/
+
+	var a int
+	var b string
+	var c, d, e int
+	var f, g, h int = 1, 2, 3
+	var i float32 = 11.4
+	var j string = "Hi! Golang!"
+	var k = 4.72
+	var l = "Hello, World!"
+	var m = true
+}
